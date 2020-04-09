9ebcfe0cdd4e8a70a72f14226115656c04a32960
Haris Khan <haris@codedazur.com>
2020-04-09 07:57:20 +0200

    change url

--- frontend/lib/apollo.js
+++ frontend/lib/apollo.js
@@ -1,9 +1,10 @@
 import { HttpLink } from "apollo-boost";
 import { withData } from "next-apollo";
+import { STRAPI_URL } from "../lib/globals";
 
 const config = {
   link: new HttpLink({
-    uri: "http://165.22.192.233:1337/graphql", // Server URL (must be absolute)
+    uri: `${STRAPI_URL}/graphql`, // Server URL (must be absolute)
     opts: {
       credentials: "same-origin" // Additional fetch() options like `credentials` or `headers`
     }
